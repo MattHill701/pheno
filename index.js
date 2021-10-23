8f40d079d6c4030c254104d45ddeccf645235415
MattHill701 <85851376+MattHill701@users.noreply.github.com>
2021-10-23 12:23:04 -0400

    wow

---
+++ index.js
@@ -0,0 +1,49 @@
+// Use the dotenv package, to create environment variables
+
+// Create a constant variable, PORT, based on what's in process.env.PORT or fallback to 3000
+// Import express, and create a server
+// Require morgan and body-parser middleware
+// Have the server use morgan with setting 'dev'
+require('dotenv').config()
+const { PORT = 3000 } = process.env
+const express = require('express');
+const server = express();
+
+const morgan = require('morgan');
+const { application_name } = require('pg/lib/defaults');
+server.use(morgan('dev'));
+
+server.use(express.json())
+
+// Import cors 
+// Have the server use cors()
+const cors = require('cors');
+server.use(cors());
+
+// Have the server use bodyParser.json()
+const bodyParser = require('body-parser')
+server.use(bodyParser.json());
+// Have the server use your api router with prefix '/api'
+const apiRouter = require('./api');
+server.use('/api', apiRouter);
+// Import the client from your db/index.js
+const { client } = require('./db/index');
+// Create custom 404 handler that sets the status code to 404.
+server.use(function (req, res, next) {
+    res.status(404).send("Oof, can't find that!")
+  })
+// Create custom error handling that sets the status code to 500
+// and returns the error as an object
+server.use(function (req, res, next) {
+  try{
+    res.status(500).send('500 error')
+  } catch (error){
+    return {message: "trouble with 500"};
+  }})
+
+// Start the server listening on port PORT
+// On success, connect to the database
+client.connect();
+server.listen(PORT, () => {
+    console.log('The server is up on port', PORT)
+  });
